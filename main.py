cc38b996230080ccdc996b3a19d4818599009ca4
Negetsu <negetsu0@gmail.com>
2025-07-23 20:21:59 -0400

    Data should be avaliable now

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hiya!")
+import pandas as pd
+from langchain_community.document_loaders.dataframe import DataFrameLoader
+excel_file = "data/Ordinances.xlsx"
+ordinances = pd.read_excel(excel_file)
+ordinances = ordinances.dropna(subset=['Unnamed: 4'])
+ordinances['Unnamed: 4'] = ordinances['Unnamed: 4'].astype(str)
+ordifix = pd.DataFrame(ordinances)
+ordiloader = DataFrameLoader(ordifix,page_content_column='Unnamed: 4')
+ordidocs = ordiloader.load()
+print("right before")
+print(ordidocs)
